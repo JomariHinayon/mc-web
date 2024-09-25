c2b5bf5bdc9f8901cd94adfb19567be15cb88500
Jomari <hinayonjomari@gmail.com>
2024-09-26 04:47:59 +0800

    put a function of the onboarding.html

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def onboarding(request):
+    return render(request, 'onboarding.html')
