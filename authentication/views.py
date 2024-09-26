30ac5dd0180ff1fbcee68129cb35bb4619cd5a25
Jomari <hinayonjomari@gmail.com>
2024-09-26 13:12:08 +0800

    Working authentications login and singup

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from authentication.forms import CustomUserCreationForm 
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-def login(request):
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        # Custom authentication with email
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials. Please try again.')
+
     return render(request, 'login.html')
 
 
+def user_signup(request):
+    form = CustomUserCreationForm()
+    
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created successfully! Please log in.')
+            return redirect('login')  # Redirect to the login page after successful registration
+        else:
+            messages.error(request, 'Please correct the errors below.')
+        
 
-def signup(request):
-    return render(request, 'register.html')
+    return render(request, 'register.html', {'form': form})
